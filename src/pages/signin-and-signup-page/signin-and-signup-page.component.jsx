8549216f7d67f2b1aa2876133238cda513c23ba0
Samiya Maity <maitysamiya@gmail.com>
2021-11-09 23:23:39 +0530

    Sign in and Sign up component is completed with firebase

--- src/pages/signin-and-signup-page/signin-and-signup-page.component.jsx
+++ src/pages/signin-and-signup-page/signin-and-signup-page.component.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './signin-and-signup-page.style.scss'
+import SignUp from '../../components/sign-up/sign_up.component'
 import SignIn from '../../components/sign-in/sign-in.component'
+
+
 
 const signinAndSignupPage = () =>(
     <div className = 'signin-and-signup-page'>
         <SignIn/>
+        <SignUp/>
+       
     </div>
 )
 
